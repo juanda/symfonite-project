dc3d2d174f0b7ff76be0a739d19f92a8186586b3
Juan David Rodriguez <juandalibaba@gmail.com>
2012-04-28 09:13:21 +0200

    reimportacion

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once dirname(__FILE__) . '/../lib/symfony/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+
+    public function setup()
+    {
+        $this->enablePlugins('sfPropelPlugin');
+        $this->enablePlugins('symfonitePlugin');
+        $this->enablePlugins('themesPlugin');
+        $this->enablePlugins('sfGuardPlugin');
+        $this->enablePlugins('sftGuardPlugin');
+        $this->enablePlugins('sfJqueryReloadedPlugin');
+        $this->enablePlugins('sfBreadNav2Plugin');
+        $this->enablePlugins('sftSAMLPlugin');
+        $this->enablePlugins('sftPAPIPlugin');
+        $this->enablePlugins('sftFedIdentMapperPlugin');
+  }
+
+}
